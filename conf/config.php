e2eeb19ca8e6fe06a5c6f691af274e456be00027
brendandebeasi <b@debea.si>
2013-04-05 22:12:04 -0700

    Login works!!!!

--- conf/config.php
+++ conf/config.php
@@ -20,8 +20,8 @@ $config = [
         'db' => [
             'host'  => 'localhost',
             'name'  => 'filesail',
-            'user'  => 'root',
-            'pass'  => 'GreatSuccess2012'
+            'user'  => 'filesail',
+            'pass'  => 'txx5bvbA5daFzZZ2'
         ],
 
         'host'      => 'http://stage.filesail.com/',
